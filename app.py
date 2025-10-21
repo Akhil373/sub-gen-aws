7baa444b9779d92c3f08a97eebc1863deebddbe4
Akhil373 <akhil2005.m@gmail.com>
2025-10-21 12:13:51 +0530

    initial changes

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
+import asyncio
+import glob
 import json
+import logging
 import os
 import shutil
-from typing import Optional
-import logging
+import subprocess
 import uuid
+from pathlib import Path
+from typing import List, Optional
 from uuid import UUID
-import yt_dlp
-from fastapi import FastAPI, File, UploadFile, HTTPException, Form
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
+
+import aioboto3
 import boto3
-from boto3.s3.transfer import S3UploadFailedError
-from botocore.client import Config, BaseClient
+import yt_dlp
+from botocore.client import BaseClient, Config
+from fastapi import FastAPI, File, Form, HTTPException, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -27,14 +32,12 @@ logging.basicConfig()
 logging.getLogger("faster_whisper").setLevel(logging.DEBUG)
 
 s3_resource: BaseClient = boto3.client(
-    "s3",
-    region_name='eu-north-1',
-    config=Config(signature_version='s3v4')
+    "s3", region_name="eu-north-1", config=Config(signature_version="s3v4")
 )
 bucket_name = "subtitle-generator-project"
 lambda_client = boto3.client("lambda", region_name="eu-north-1")
-dynamodb = boto3.resource('dynamodb', region_name="eu-north-1")
-status_table = dynamodb.Table('SubtitleJobStatus')
+dynamodb = boto3.resource("dynamodb", region_name="eu-north-1")
+status_table = dynamodb.Table("SubtitleJobStatus")
 
 
 def youtube_download_video(VIDEO_URL, DOWNLOAD_DIR, output_template):
@@ -42,14 +45,14 @@ def youtube_download_video(VIDEO_URL, DOWNLOAD_DIR, output_template):
     os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
     ydl_opts = {
-        'outtmpl': output_template,
-        'format': 'bestvideo[height<=1080]+bestaudio/best',
-        'http_headers': {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        "outtmpl": output_template,
+        "format": "bestvideo[height<=1080]+bestaudio/best",
+        "http_headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         },
-        'merge_output_format': 'mp4',
-        'verbose': True,
-        'cookiefile': 'youtube_cookies.txt'
+        "merge_output_format": "mp4",
+        "verbose": True,
+        "cookiefile": "youtube_cookies.txt",
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -60,19 +63,23 @@ def youtube_download_video(VIDEO_URL, DOWNLOAD_DIR, output_template):
                 return None
 
             final_filepath = None
-            if 'requested_downloads' in info and info['requested_downloads']:
-                final_filepath = info['requested_downloads'][0]['filepath']
-            elif '_filename' in info:
-                final_filepath = info['_filename']
+            if "requested_downloads" in info and info["requested_downloads"]:
+                final_filepath = info["requested_downloads"][0]["filepath"]
+            elif "_filename" in info:
+                final_filepath = info["_filename"]
             else:
-                print("Warning: yt-dlp did not provide a clear filepath. Attempting to construct.")
-                if 'title' in info and 'ext' in info:
+                print(
+                    "Warning: yt-dlp did not provide a clear filepath. Attempting to construct."
+                )
+                if "title" in info and "ext" in info:
                     guessed_filename = f"{info['title']}.{info['ext']}"
                     guessed_path = os.path.join(DOWNLOAD_DIR, guessed_filename)
                     if os.path.exists(guessed_path):
                         final_filepath = guessed_path
                     else:
-                        print(f"Could not determine downloaded file path for {URLS[0]}.")
+                        print(
+                            f"Could not determine downloaded file path for {URLS[0]}."
+                        )
                         return None
 
             return final_filepath
@@ -80,6 +87,7 @@ def youtube_download_video(VIDEO_URL, DOWNLOAD_DIR, output_template):
         except Exception as e:
             print(f"An error occurred during YouTube download: {e}")
             return None
+
 
 def clean_files(path, zip_file, video_path):
     try:
@@ -95,104 +103,281 @@ def clean_files(path, zip_file, video_path):
     except Exception as err:
         print("Error clearing files: ", err)
 
-async def store_in_s3(file: UploadFile):
-    job_id = uuid.uuid4()
-    s3_key = f"uploads/{job_id}/{file.filename}"
+
+def get_file_extension(file_path: str) -> str:
+    return Path(file_path).suffix.lower()
+
+
+async def upload_chunks_to_s3(chunk_files: list, job_id: str, file_extension: str):
+    async with aioboto3.Session().client("s3") as s3:
+        tasks = []
+
+        for index, chunk_file in enumerate(chunk_files):
+            s3_key = f"chunks/{job_id}/chunk_{index:03d}{file_extension}"
+            task = s3.upload_file(chunk_file, bucket_name, s3_key)
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+
+
+def split_file_into_chunks(
+    filepath: str, job_id: str, chunk_duration: int = 60
+) -> List[str]:
+    original_ext = get_file_extension(filepath)
+    output_pattern = f"/tmp/audio/{job_id}/chunk_%03d{original_ext}"
     try:
-        await file.seek(0)
-        s3_resource.upload_fileobj(file.file, bucket_name, s3_key)
-        print(f"Uploaded {file.filename} to {bucket_name}")
-        return job_id, s3_key
-    except S3UploadFailedError as err:
-        raise HTTPException(status_code=400, detail=f"Error when uploading to S3: {err}")
+        os.makedirs(f"/tmp/audio/{job_id}", exist_ok=True)
+        cmd = [
+            "ffmpeg",
+            "-i",
+            filepath,
+            "-f",
+            "segment",
+            "-segment_time",
+            str(chunk_duration),
+            "-reset_timestamps",
+            "1",
+            "-c",
+            "copy",
+            output_pattern,
+        ]
+        subprocess.run(cmd, check=True)
+
+        chunk_files = sorted(glob.glob(f"/tmp/audio/{job_id}/chunk_*{original_ext}"))
+        return chunk_files
+    except Exception as e:
+        raise HTTPException(
+            status_code=400, details=f"Error whe splitting file: {e}"
+        ) from e
 
 
-@app.get('/test')
+def trigger_lambda(job_id: str, total_chunks: int):
+    for chunk_index in range(total_chunks):
+        response = lambda_client.invoke(
+            FunctionName="subtitle-gen-lambda",
+            InvocationType="Event",
+            Payload=json.dumps(
+                {
+                    "job_id": job_id,
+                    "chunk_index": chunk_index,
+                    "total_chunks": total_chunks,
+                    "s3_key": f"chunks/{job_id}/chunk_{chunk_index:03d}.mp4",
+                    "bucket_name": bucket_name,
+                }
+            ),
+        )
+    return response
+
+def add_subtitles(media_path, subtitle_file):
+    base, ext = os.path.splitext(os.path.basename(media_path))
+    dir_path = os.path.dirname(media_path)
+
+    final_output = os.path.join(dir_path, f"{base}_subtitled.mp4")
+
+    if not os.path.exists(subtitle_file):
+        print(f"Error: Subtitle file not found at {subtitle_file}")
+        return None
+
+    video_formats = [".mp4", ".webm", ".mpeg"]
+
+    try:
+        if ext.lower() in video_formats:
+            print("Found video file.")
+
+            temp_output = os.path.join(dir_path, f"{base}_temp.mp4")
+            cmd = [
+                "ffmpeg",
+                "-i",
+                media_path,
+                "-i",
+                subtitle_file,
+                "-c",
+                "copy",
+                "-c:s",
+                "mov_text",
+                temp_output,
+                "-y",
+            ]
+
+            subprocess.run(cmd, check=True, capture_output=True)
+
+            if ext.lower() == ".mp4":
+                os.remove(media_path)
+                os.rename(temp_output, media_path)
+            else:
+                os.rename(temp_output, final_output)
+        else:
+            print("Found audio file.")
+            temp_video = os.path.join(dir_path, f"{base}_temp.mp4")
+            cmd1 = [
+                "ffmpeg",
+                "-f",
+                "lavfi",
+                "-i",
+                "color=c=black:s=1280x720:r=5",
+                "-i",
+                media_path,
+                "-c:a",
+                "copy",
+                "-shortest",
+                temp_video,
+                "-y",
+            ]
+            subprocess.run(cmd1, check=True, capture_output=True)
+
+            cmd2 = [
+                "ffmpeg",
+                "-i",
+                temp_video,
+                "-i",
+                subtitle_file,
+                "-c",
+                "copy",
+                "-c:s",
+                "mov_text",
+                final_output,
+                "-y",
+            ]
+            subprocess.run(cmd2, check=True, capture_output=True)
+            os.remove(temp_video)
+
+        return final_output
+
+    except subprocess.CalledProcessError as e:
+        print(f"FFmpeg Error: {e.stderr.decode()}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def create_srt_file()
+
+def combine_transcriptions()
+
+def assemble_final_video()
+
+
+@app.get("/test")
 async def test_endpoint():
     return {"message": "FastAPI is working!"}
 
 
-@app.post('/generate-subtitles')
+@app.post("/generate-subtitles")
 async def generate_subtitles(
-        file: Optional[UploadFile] = File(None),
-        youtube_url: Optional[str] = Form(None)
+    file: Optional[UploadFile] = File(None), youtube_url: Optional[str] = Form(None)
 ):
-    upload_dir = '/tmp/audio'
+    upload_dir = "/tmp/audio"
     os.makedirs(upload_dir, exist_ok=True)
-    job_id: UUID | None = None
-    s3_key: str | None = None
+    job_id: UUID = uuid.uuid4()
+    filepath: str | None = None
 
     if file:
-        job_id, s3_key = await store_in_s3(file)
-
+        filepath = Path(upload_dir) / file.filename
+        with open(filepath, "wb", encoding="utf-8") as f:
+            shutil.copyfileobj(file.file, f)
+        print(f"File saved successfully in {filepath}")
     elif youtube_url:
         output_template = os.path.join(upload_dir, "%(title)s.%(ext)s")
         filepath = youtube_download_video(youtube_url, upload_dir, output_template)
 
         if filepath is None or not os.path.exists(filepath):
-            raise HTTPException(status_code=400, detail="Failed to download YouTube video")
-
-        filename = os.path.basename(filepath)
-        job_id: UUID = uuid.uuid4()
-        job_id_str: str = str(job_id)
-        s3_key: str = f"uploads/{job_id_str}/{filename}"
-        s3_resource.upload_file(filepath, bucket_name, s3_key)
+            raise HTTPException(
+                status_code=400, detail="Failed to download YouTube video"
+            )
     else:
-        raise HTTPException(status_code=400, detail="You must provide either a file or youtube URL.")
+        raise HTTPException(
+            status_code=400, detail="You must provide either a file or youtube URL."
+        )
 
     job_id_str: str = str(job_id)
-    payload = {
-        's3_key': s3_key,
-        'job_id': job_id_str,
-        'bucket_name': bucket_name
-    }
+
+    status_table.put_item(
+        Item={
+            "job_id": job_id,
+            "chunk_index": -1,
+            "status": "STARTED",
+            "total_chunks": 0,
+        }
+    )
+    chunk_files = split_file_into_chunks(filepath, job_id_str)
+    file_extension = get_file_extension(filepath)
+    await upload_chunks_to_s3(chunk_files, job_id, file_extension)
+
     try:
-        response = lambda_client.invoke(
-            FunctionName='subtitle-gen-lambda',
-            InvocationType='Event',
-            Payload=json.dumps(payload)
-        )
+        response = trigger_lambda(job_id, len(chunk_files))
         print("Lambda invoked successfully:", response)
-        return JSONResponse(
-            status_code=202,
-            content={"message": "Processing started", "job_id": job_id_str}
-        )
 
     except Exception as e:
-        print(f'Error invoking Lambda: {e}')
-        status_table.put_item(Item={
-            'job_id': job_id_str,
-            'status': 'FAILED',
-            'message': str(e)
-        })
-        raise HTTPException(status_code=500, detail=f"Job initialization failed: {str(e)}")
+        print(f"Error invoking Lambda: {e}")
+        status_table.put_item(
+            Item={"job_id": job_id_str, "status": "FAILED", "message": str(e)}
+        )
+        raise HTTPException(
+            status_code=500, detail=f"Job initialization failed: {str(e)}"
+        ) from e
+
+    status_table.update_item(
+        Key={"job_id": job_id_str, "chunk_index": -1},
+        UpdateExpression="SET total_chunks = :tc",
+        ExpressionAttributeValues={":tc": len(chunk_files)},
+    )
+    return {"job_id": job_id, "status": "started"}
 
 
-@app.get('/job-status/{job_id}')
+@app.get("/job-status/{job_id}")
 def check_job_status(job_id: str):
-    response = status_table.get_item(Key={'job_id': job_id})
-
-    if 'Item' not in response:
+    response = status_table.query(
+        KeyConditionExpression=boto3.dynamodb.conditions.Key("job_id").eq(job_id)
+    )
+    if "Items" not in response or len(response["Items"]) == 0:
         raise HTTPException(status_code=404, detail="Job ID not found")
 
-    item = response['Item']
-    curr_status = item['status']
+    items = response["Items"]
+    overall_job = None
+    chunk_statuses = []
+
+    for item in items:
+        if item.get("chunk_index") == -1:
+            overall_job = item
+        else:
+            chunk_statuses.append(item)
+
+    if not overall_job:
+        raise HTTPException(status_code=404, detail="Job not found")
+
+    total_chunks = overall_job.get("total_chunks", 0)
+    completed_chunks = sum(
+        1 for chunk in chunk_statuses if chunk.get("status") == "COMPLETED_CHUNK"
+    )
+    failed_chunks = sum(
+        1 for chunk in chunk_statuses if chunk.get("status") == "FAILED_CHUNK"
+    )
 
     response_body = {
         "job_id": job_id,
-        "status": curr_status,
+        "status": overall_job.get("status", "PROCESSING"),
+        "progress": f"{completed_chunks}/{total_chunks}",
+        "completed_chunks": completed_chunks,
+        "total_chunks": total_chunks,
+        "failed_chunks": failed_chunks,
     }
 
-    if curr_status == 'COMPLETED':
-        result_key = item['result_key']
+    if (completed_chunks >= total_chunks and overall_job.get("status") not in ["ASSEMBLING", "COMPLETED"]):
+        import threading
+        thread = threading.Thread(target=assemble_final_video, args=(job_id, total_chunks))
+        thread.daemon = True
+        thread.start()
+
+        response_body["status"] = "ASSEMBLING"
+
+    elif overall_job.get("status") == "COMPLETED":
+        result_key = overall_job.get('result_key')
         try:
             presigned_url = s3_resource.generate_presigned_url(
-                'get_object',
-                Params={'Bucket': bucket_name, 'Key': result_key},
-                ExpiresIn=3600
+                "get_object",
+                Params={"Bucket":bucket_name, "Key": result_key},
+                ExpiresIn = 3600,
             )
-            response_body["download_url"] = presigned_url
+            response_body['download_url']= presigned_url
         except Exception as e:
-            raise HTTPException(status_code=400, detail=str(e))
-
+            raise HTTPException(status_code=400, detail=str(e)) from e
     return JSONResponse(response_body)
